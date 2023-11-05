9fa7c0ca96e0cf8af4cba57a9fd3b25ae59795c9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-11-05 23:51:06 +0500

    rename: button->index, style

--- src/components/genericUi/button/index.tsx
+++ src/components/genericUi/button/index.tsx
@@ -1,11 +1,12 @@
+import '../../../assets/styles/touchAnimate.scss'
 import { ButtonProps } from '../../../interface/button.interface'
-import './button.scss'
+import './style.scss'
 
 export const Button = (props: ButtonProps) => {
 	const { children, option, color, ...rest } = props
-	
-	const collectClass = `button button-${color}-${option}`
-	
+
+	const collectClass = `touchAnimate button  button-${color}-${option}`
+
 	return (
 		<button {...rest} className={collectClass}>
 			{children}
