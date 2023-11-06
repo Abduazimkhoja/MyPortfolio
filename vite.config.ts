068c516b060f4dfe6bf4e7b81b3b13cdbdd86d72
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-11-06 16:18:44 +0500

    add: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
+   resolve: {
+      alias: {
+         // '@': path.resolve(__dirname, './src'),
+         '@ui': path.resolve(__dirname, './src/components/ui'),
+         '@screens': path.resolve(__dirname, './src/components/screens'),
+         '@hooks': path.resolve(__dirname, './src/hooks'),
+         '@interface': path.resolve(__dirname, './src/interface'),
+         '@pages': path.resolve(__dirname, './src/pages'),
+         '@components': path.resolve(__dirname, './src/components'),
+         '@styles': path.resolve(__dirname, './src/assets/styles')
+      }
+   }
 })
